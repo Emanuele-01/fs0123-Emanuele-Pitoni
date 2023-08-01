4dbb17b86eb71a87b90c379283f30ba1bd55baf9
Emanuele-01 <emanuele.pitoni.2001@gmail.com>
2023-08-01 15:34:00 +0200

    update unit 3 week 1 day 1

---
+++ m3/s1/g1/src/App.jsx
@@ -0,0 +1,29 @@
+import logo from './assets/logo.svg';
+import './App.css';
+import ButtonComponent from './components/ButtonComponent';
+import ImageComponent from './components/ImageComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <ImageComponent src="https://kinsta.com/it/wp-content/uploads/sites/2/2022/07/what-is-react-js-feature-image.png " alt="Foto React" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <ButtonComponent id="button" content="Invia" />
+      </header>
+    </div>
+  );
+}
+
+export default App;

7e2a39bd39f9cb0c00891f0510466c4c58609943
Emanuele-01 <emanuele.pitoni.2001@gmail.com>
2023-08-01 15:39:20 +0200

    update unit 3 week 1 day 3

---
+++ m3/s1/g3/book-app-2/src/components/SingleBook.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import {Button, Card, Col} from 'react-bootstrap';
+
+class SingleBook extends Component {
+    state = {
+        selected : false
+    }
+    
+    render(){
+        const cardColor = this.state.selected ? "border-3 border-danger " : "";
+        return(
+            <Col xs={3} className="mt-4">
+                <Card className={cardColor} key={this.props.Key}>
+                <Card.Img className="h-50" variant="top" src= {this.props.src} alt={this.props.alt}/>
+                <Card.Body className="d-flex flex-column">
+                    <Card.Title><strong>{this.props.title}</strong></Card.Title>
+                    <Card.Text className="mt-auto fs-4">{this.props.category} <p>{this.props.asin}</p> </Card.Text>
+                    <Button className="mt-auto" variant="danger">{this.props.price} €</Button>
+                </Card.Body>
+                </Card>
+            </Col>
+        )
+    }
+}
+
+export default SingleBook;
